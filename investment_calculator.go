6cf06e0d6a811157071aaeb9b92932ffbd7821a9
Palanisarathi <palaniparthasarathi@gmail.com>
2024-04-24 21:31:41 +0530

    type cast

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,9 +1,15 @@
 package main
 
-func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+import (
+	"fmt"
+	"math"
+)
 
-	var futureValue = investmentAmount * (1 + expectedReturnRate/100)
+func main() {
+	var investmentAmount float64 = 1000
+	var expectedReturnRate float64 = 5.5
+	var years float64 = 10
+
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	fmt.Println(futureValue)
 }
