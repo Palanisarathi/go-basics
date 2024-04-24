59db9711a8794e81c4f9055cf2597f05c06ff063
Palanisarathi <palaniparthasarathi@gmail.com>
2024-04-24 21:35:57 +0530

    another way

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	var investmentAmount float64 = 1000
+	years, expectedReturnRate := 10.0, 5.5
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Println(futureValue)
 }
