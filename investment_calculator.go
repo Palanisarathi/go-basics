4e2803f0cdc6287c6901b1a43ea035d9fe41b8ec
Palanisarathi <palaniparthasarathi@gmail.com>
2024-04-29 20:46:39 +0530

    function with parameters

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,19 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+const inflationRate float64 = 2.5
+
 func main() {
-	// const inflationRate float64 = 2.5
-	// var investmentAmount float64
-	// var years float64
-	// expectedReturnRate := 5.5
-	// fmt.Print("Investment Amount: ")
-	// fmt.Scan(&investmentAmount)
-	// fmt.Print("Years: ")
-	// fmt.Scan(&years)
-	// fmt.Print("Expected Return Rate: ")
-	// fmt.Scan(&expectedReturnRate)
-	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-	// fmt.Println(futureValue)
-	// fmt.Println((futureRealValue))
-	profit_calculator()
+
+	var investmentAmount float64
+	var years float64
+	var name string
+	expectedReturnRate := 5.5
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	//futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	//futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+	fmt.Printf("investement %.1f", investmentAmount)
+	fmt.Print("Enter your name")
+	fmt.Scan(&name)
+	outputText(name)
+	//profit_calculator()
+}
+func outputText(text string) {
+	fmt.Println("hello" + text)
+}
+func calculateFutureValues(investmentAmount float64, expectedReturnRate float64, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	frv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, frv
 }
